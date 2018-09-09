445ecba9f5e4841036a8cf9e1a79cc8fd01da1d7
NYJOBS <berrymelon@teamblind.com>
2018-09-08 23:41:03 -0700

    Initial Commit

---
+++ VIPERTodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPERTodo
+//
+//  Created by Doheny Yoon on 9/8/18.
+//  Copyright © 2018 Doheny Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
